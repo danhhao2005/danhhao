940e997e6b3d34926f08cd629201d4d1edb24910
danhhao2005 <23021544@vnu.edu.vn>
2024-03-07 21:05:38 +0700

    nop bai  tap tuan 3

---
+++ BT03/bai1.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct point {
+int x, y;
+};
+void printPoint (point p){
+cout << p.x << p.y;
+}
+int main(){
+point myPoint;
+myPoint.x = 10;
+myPoint.y = 20;
+printPoint(myPoint);
+return 0;
+}
