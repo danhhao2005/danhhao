91d85f2c9ba02db825b61d6cedffbe84f88ece66
danhhao2005 <23021544@vnu.edu.vn>
2024-04-22 16:24:15 +0700

    Update bai4a.cpp

--- BT10/bai4a.cpp
+++ BT10/bai4a.cpp
@@ -3,6 +3,6 @@ using namespace std;
 int main(){
    int a=4;
     int *p=&a;
-   delete p;//khi chay thi no khong hien loi nhung co ve nhu ta da truy cap vao vung nho khong hop le
+   delete p;
 
 }
